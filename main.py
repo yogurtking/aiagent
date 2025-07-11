5c0c12578e6d20fc19953f62941baa11863d3130
Yogurtking <yogurtking@gmail.com>
2025-07-11 17:23:55 -0400

    Initial commit, prompting as command line argument

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+def main():
+   
+
+    if len(sys.argv) < 2:
+        print("Usage: python my_script.py 'your prompt here'")
+        sys.exit(1)  
+        
+    #print("enter your prompt for the AI: ")
+    #prompt = input()
+    #prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=sys.argv[1]
+    )
+    #print("Hello from aiagent!")
+    print(response.text)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+if __name__ == "__main__":
+    main()
